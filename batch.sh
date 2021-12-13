8c7547ab456730f8f8f1bd2e0bef5f2e50bf2fb4
Dovydas Martinkus <dovydas.martinkus@gmail.com>
2021-12-13 16:15:15 +0200

    Files

--- batch.sh
+++ batch.sh
@@ -4,8 +4,5 @@
 #SBATCH --output=results.%j.out
 #SBATCH --error=results.%j.err
 
-
-echo "started on $(date)"
 echo -e "searching for hamiltonian paths in $1\n"
 mpirun python3 hamilton.py "$1"
-echo "ended on $(date)"
